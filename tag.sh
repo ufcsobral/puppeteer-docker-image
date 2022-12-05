ff253d1fbfc2a7eb73faf1dbd641c2af50ab7f9b
Almino Melo <almino@sobral.ufc.br>
2022-12-05 11:54:33 -0300

    Foi pro registro Docker local

--- tag.sh
+++ tag.sh
@@ -3,7 +3,7 @@
 set -eux
 
 REGISTRY=200.129.37.136:5000
-IMAGE_NAME=puppeteer
+IMAGE_NAME=puppeteer:${1:-2}
 
 sudo docker build $(dirname $0) -t $IMAGE_NAME
 sudo docker tag $IMAGE_NAME $REGISTRY/$IMAGE_NAME
